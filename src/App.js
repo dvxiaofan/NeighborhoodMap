a0b907dbd1b8a75e74dcf6a12d410688659f11ac
xiaofan <1353815633@qq.com>
2018-07-09 16:11:21 +0800

    创建菜单和地图组件

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import NavMenu from './NavMenu';
+import Map from './Map';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        <NavMenu />
+        <Map />
       </div>
     );
   }
---
+++ src/NavMenu.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+
+class NavMenu extends Component {
+
+	render() {
+
+		return (
+			<div className='NavMenu'>NavMenu</div>
+		)
+	}
+}
+
+export default NavMenu;
