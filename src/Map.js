4fe4fe70350b9f243fbe78b3c75987ab5038a0fe
xiaofan <1353815633@qq.com>
2018-07-10 17:47:05 +0800

    显示多个标记点

--- src/Map.js
+++ src/Map.js
@@ -3,23 +3,30 @@ import { Map, Marker } from 'react-amap';
 import './App.css';
 
 
+const markers = [
+	{ longitude: 114.308872, latitude: 30.613611 },
+	{ longitude: 114.298072, latitude: 30.57819 },
+	{ longitude: 114.294638, latitude: 30.592081 },
+	{ longitude: 114.31335, latitude: 30.602572 },
+	{ longitude: 114.269919, latitude: 30.594741 }
+];
+
 class GMap extends Component {
 	constructor() {
 		super();
 
 		this.state = {
 			mapCenter: { longitude: 114.288959, latitude: 30.615088 },
-			marker: { longitude: 114.308872, latitude: 30.613611}
 		}
-
 	}
 
 	render() {
 		return (
 			<div className='Map'>
-				<Map amapkey='fb16aeca0f532bba128fea350d352d09' center={this.mapCenter} zoom='12'>
-					<Marker position={this.state.marker}/>
-
+				<Map amapkey='fb16aeca0f532bba128fea350d352d09' center={this.state.mapCenter} zoom='12'>
+						{markers.map(mar => (
+							<Marker position={mar} />
+						))}
 				</Map>
 			</div>
 		)
