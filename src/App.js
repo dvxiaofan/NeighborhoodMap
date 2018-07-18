0fea9ef6f776dd2ae8249ed99841684354412781
xiaofan <1353815633@qq.com>
2018-07-18 16:21:40 +0800

    暂时改用Google地图

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
 import React, { Component } from 'react';
 import './App.css';
 import { 
-	MAP_KEY,
+  GOOGLEMAP_KEY,
  } from './constants.js';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {}
+    this.state = {
+      map: '',
+      markers: '',
+      locations: [
+        {
+          id: '1',
+          title: '11',
+          address: '111',
+          location: { lat: 30.613611, lng: 114.308872 },
+          type: '1111'
+        },{
+          id: '2',
+          title: '22',
+          address: '222',
+          location: { lat: 30.57819, lng: 114.298072 },
+          type: '2222'
+        },{
+          id: '3',
+          title: '33',
+          address: '333',
+          location: { lat: 30.592081, lng: 114.294638 },
+          type: '3333'
+        },{
+          id: '4',
+          title: '44',
+          address: '444',
+          location: { lat: 30.602572, lng: 114.31335 },
+          type: '4444'
+        },{
+          id: '5',
+          title: '55',
+          address: '555',
+          location: { lat: 30.594741, lng: 114.269919 },
+          type: '5555'
+        }
+      ]
+    };
   }
 
   componentDidMount() {
+    window.initMap = this.initMap;
     
+    loadGoogleMap(`https://maps.googleapis.com/maps/api/js?key=${GOOGLEMAP_KEY}&callback=initMap`);
   }
 
+  initMap() {
+
+    var mapview = document.getElementById("map");
+    var map = new window.google.maps.Map(mapview, {
+      zoom: 13,
+      center: {
+        lat: 30.554393,
+        lng: 114.308543
+      },
+      
+      mapTypeControl: true
+    });
+    
+  }
+  
   render() {
     return (
       <div className="App">
         <div className='NavMenu'>NavMenu</div>
-        <div className='Map'></div>
+        <div className='Map' id='map'></div>
       </div>
     );
   }
@@ -27,12 +80,14 @@ class App extends Component {
 
 export default App;
 
-
-/* const markers = [
-	{ longitude: 114.308872, latitude: 30.613611 },
-	{ longitude: 114.298072, latitude: 30.57819 },
-	{ longitude: 114.294638, latitude: 30.592081 },
-	{ longitude: 114.31335, latitude: 30.602572 },
-	{ longitude: 114.269919, latitude: 30.594741 }
-];
- */
+//  加载google地图
+function loadGoogleMap(src) {
+  var script = window.document.createElement('script');
+  var ref = window.document.getElementsByTagName('script')[0];
+  script.src = src;
+  script.async = true;
+  script.onerror = () => {
+    document.write('地图无法加载');
+  };
+  ref.parentNode.insertBefore(script, ref);
+}
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 
 
-export const MAP_KEY = 'fb16aeca0f532bba128fea350d352d09';
+export const GMAP_KEY = 'fb16aeca0f532bba128fea350d352d09';
+export const GOOGLEMAP_KEY = 'AIzaSyBiYvDuNhypAzH45rZPDhKEsGtuaB90mtQ';
